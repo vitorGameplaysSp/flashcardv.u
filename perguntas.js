830211c76d821c12dd84f57be1512e092d44664d
vitorGameplaysSp <0000110668476xsp@al.educacao.sp.gov.br>
2024-11-28 18:17:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,27 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital da Espanha?',
+    'Madrid',
+    )
+    
+    criaCartao(
+    'Geografia',
+    'Qual é a capital da Belgica?'
+    'Bruxelas'
+)
+
+criaCartao(
+    'Geografia'
+    'Qual é a capital de Portugal'
+    'Lisboa'
+)
+
+criaCartao(
+    'Geografia'
+    'Qual é a capital da Ucrania
+    'Kiev'
+
+
+)
+
+
